650859fb2c268e809ccf693836dfc1482df37996
sanket <sanketsabale9767@gmail.com>
2023-01-26 13:50:18 +0530

    Understand model

--- TodoApp/src/main/java/com/sanketcode/spring/TodoApp/login/LoginController.java
+++ TodoApp/src/main/java/com/sanketcode/spring/TodoApp/login/LoginController.java
@@ -1,14 +1,20 @@
 package com.sanketcode.spring.TodoApp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class LoginController {
 	
+	//http://localhost:8080/login?name=sanket
+	//Modal
 	@RequestMapping("login")
-	public String gotoLoginPage() {
+	public String gotoLoginPage(@RequestParam String name,ModelMap model) {
+		model.put("name", name);
+		System.out.println("Request param is "+name);
 		return "login";
 	}
 	
--- TodoApp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ TodoApp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,9 @@
 		<title> Login Page</title>
 	</head>
 	<body>
+	
 		Welcome to the login page!
+		
+		${name}
 	</body>
 </html>
