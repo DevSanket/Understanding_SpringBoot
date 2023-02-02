39028f8c9560c8443828c72f4681698ec754d23e
Programmer Sanket <55836873+DevSanket@users.noreply.github.com>
2023-02-02 19:01:03 +0530

    Handling UserNotFoundException

--- restful-web-services/src/main/java/com/sanketscode/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/sanketscode/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,12 +1,17 @@
 package com.sanketscode.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+//import jakarta.servlet.Servlet;
 
 @RestController
 public class UserResource {
@@ -23,12 +28,23 @@ public class UserResource {
 	
 	@GetMapping("/users/{id}")
 	public User retriveUser(@PathVariable int id){
-		return service.findOne(id);
+		User user = service.findOne(id);
+		
+		if(user == null) {
+			throw new UserNotFoundException("id : "+id);
+		}
+		
+		return user;
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		service.save(user);
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri(); // users/{id} 
+		return ResponseEntity.created(location ).build();
 		
 	}
 	
